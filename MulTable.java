95957505c89b5115421e5302fd55320b730559b2
Vivek Adam <70575628+adamvivek@users.noreply.github.com>
2023-03-20 14:01:20 +0530

    Add files via upload

---
+++ MulTable.java
@@ -0,0 +1,11 @@
+public class MulTable {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        for(int i = 1; i <= 10; ++i)
+        {
+            System.out.printf(" %d \n", num * i);
+        }
+    }
+}
